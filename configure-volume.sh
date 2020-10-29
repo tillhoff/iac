88e44a54552dfca73faf6b2fed2a51c2b4edd217
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-29 04:23:21 +0100

    added volume configuration

---
+++ configure-volume.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $EUID != 0 ]]; then
+  <&2 echo "You're not root. This script isn't meant for that."
+  exit
+fi
+
+devvolumename="HC_Volume_6753628"
+mntvolumename="volume"
+
+mkdir /mnt/volume
+
+mount -o discard,defaults /dev/disk/by-id/scsi-0$devvolumename /mnt/$mntvolumename
+
+cat /etc/fstab | grep $devvolumename
+if [[] $? = 0 ]]; then
+  echo "/dev/disk/by-id/scsi-0$devvolumename /mnt/$mntvolumename ext4 discard,nofail,defaults 0 0" >> /etc/fstab
+fi
