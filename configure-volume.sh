778dd6ee6ca13767652f5f69102ebb9fef07a308
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-29 09:14:41 +0100

    reworked volume configuration script

--- configure-volume.sh
+++ configure-volume.sh
@@ -8,11 +8,10 @@ fi
 devvolumename="HC_Volume_6753628"
 mntvolumename="volume"
 
-mkdir -p /mnt/volume
+mkdir -p /mnt/$mntvolumename
 
 mount -o discard,defaults /dev/disk/by-id/scsi-0$devvolumename /mnt/$mntvolumename || true
 
-cat /etc/fstab | grep $devvolumename
-if [[ $? = 0 ]]; then
+if [ "$(cat /etc/fstab | grep $devvolumename)" == "" ]; then
   echo "/dev/disk/by-id/scsi-0$devvolumename /mnt/$mntvolumename ext4 discard,nofail,defaults 0 0" >> /etc/fstab
 fi
