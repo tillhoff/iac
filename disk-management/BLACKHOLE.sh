37e0fa6082d5d6f42afc51775a8215f1efe8e00c
Till Hoffmann <till.hoffmann@enforge.de>
2020-11-16 18:47:40 +0100

    added logs

--- disk-management/BLACKHOLE.sh
+++ disk-management/BLACKHOLE.sh
@@ -8,6 +8,7 @@ fi
 # DATA-SSD pool
 NAME="data-ssds"
 DISKID="ata-Samsung*"
+printf "Started working on pool $NAME\n"
 if sudo zpool status -v | grep $NAME >/dev/null; then
   DISKS=$(ls /dev/disk/by-id/$DISKID)
   for DISK in ${DISKS[@]}; do sudo sgdisk --zap-all $DISK; done # clear all partitions from disks
@@ -15,9 +16,11 @@ if sudo zpool status -v | grep $NAME >/dev/null; then
   sudo zpool create -m /mnt/$NAME -f $NAME raidz ${DISKS[*]} # create pool
   sudo chown enforge:enforge /mnt/$NAME #  set permission on mount folder
 fi
+printf "Finished working on pool $NAME\n"
 
 NAME="data-hdds"
 DISKID="ata-WDC*"
+printf "Started working on pool $NAME\n"
 if sudo zpool status -v | grep $NAME >/dev/null; then
   DISKS=$(ls /dev/disk/by-id/$DISKID)
   for DISK in ${DISKS[@]}; do sudo sgdisk --zap-all $DISK; done # clear all partitions from disks
@@ -25,12 +28,15 @@ if sudo zpool status -v | grep $NAME >/dev/null; then
   sudo zpool create -m /mnt/$NAME -f $NAME raidz ${DISKS[*]} # create pool
   sudo chown enforge:enforge /mnt/$NAME #  set permission on mount folder
 fi
+printf "Finished working on pool $NAME\n"
 
 sudo zpool status -v
 
+printf "Started setting spin-down timout"
 ALLDISKS=$(ls "/dev/sd*")
 for DISK in ${ALLDISKS[@]}; do
   if [[ "$(cat /sys/block/$DISK/queue/rotational)" == "1" ]]; then
     sudo hdparm -S 120 /dev/$DISK # set disk timeout to 10min (lookup calculation when adjusting!)
   fi
 done
+printf "Finished setting spin-down timout"
