71df591dc3b5ff61ecde72f713409542d8ddc244
Till Hoffmann <till.hoffmann@enforge.de>
2020-11-19 01:37:36 +0100

    further improved hdparm stuff

--- disk-management/BLACKHOLE.sh
+++ disk-management/BLACKHOLE.sh
@@ -43,7 +43,7 @@ printf "Finished working on pool $NAME.\n"
 sudo zpool status -v
 
 printf "Started setting spin-down timeout (on rotating disks only).\n"
-ALLDISKS=$(ls /dev/sd*)
+ALLDISKS=$(ls /dev/sd?)
 for DISK in ${ALLDISKS[@]}; do
   if [[ "$(cat /sys/block/${DISK#/dev/}/queue/rotational)" == "1" ]]; then
     sudo hdparm -S 242 $DISK # set disk timeout to 1h (lookup calculation when adjusting!)
